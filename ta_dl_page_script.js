b363b68ddfa0e484cb9f445e2e0401b654cc09bc
Daniel Jue <djue@phy6.net>
2024-01-07 12:33:00 -0500

    Create ta_dl_page_script.js
    Same script that is in the README

---
+++ ta_dl_page_script.js
@@ -0,0 +1,16 @@
+// Custom :contains selector
+function contains(selector, text) {
+    const elements = document.querySelectorAll(selector);
+    return Array.from(elements).filter(element => RegExp(text).test(element.textContent));
+}
+
+// Get all buttons with the text "Download now"
+const downloadButtons = contains('button', 'Download now');
+
+// Click each button with a delay of 1 second
+downloadButtons.forEach((button, index) => {
+    setTimeout(() => {
+        button.click();
+        console.log(`Clicked button with text: "${button.textContent}" and id: "${button.id}"`);
+    }, index * 200); // 1000 milliseconds = 1 second
+});
